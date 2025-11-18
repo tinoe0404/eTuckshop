79469f58a7d258852aff34552d16a2dcca4cb1a3
tinoe0404 <tinoe044@gmail.com>
2025-11-18 09:00:48 +0200

    created the getAllProducts function

--- backend/src/controllers/product.controller.ts
+++ backend/src/controllers/product.controller.ts
@@ -1,2 +1,22 @@
+import { Context } from "hono";
+import { prisma } from "../utils/db";
+import { getStockLevel } from "../utils/stock";
 
-
+// ==============================
+// GET ALL PRODUCTS
+// ==============================
+export const getAllProducts = async (c: Context) => {
+    const products = await prisma.product.findMany({
+      include: {
+        category: true,
+      },
+    });
+  
+    const updated = products.map((p) => ({
+      ...p,
+      stockLevel: getStockLevel(p.stock),
+    }));
+  
+    return c.json(updated);
+  };
+  
